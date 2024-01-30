059eb75f0bb7ccd79376188d7b334bdc13f9d199
Almir <aucanbarlic@axistms.net>
2024-01-30 17:27:06 +0100

    stat component changed

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
         <Cardtag variant="grass">Grass</Cardtag>
         <Button>Grass</Button>
         <div className="max-w-xs">
-          <Cardprop value="888">Attack</Cardprop>
+          <Cardprop value="888" stat={'Attack'} />
         </div>
       </div>
     </div>
