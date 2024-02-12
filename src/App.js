4d3749eb44d22c19cebb6eca740a019c95157749
Almir <aucanbarlic@axistms.net>
2024-02-12 13:38:00 +0100

    resolving conflict

--- src/App.js
+++ src/App.js
@@ -5,11 +5,8 @@ import { Header } from './components/Header';
 import { Cardtag } from './components/ui/Cardtag';
 import { Button } from './components/ui/Button';
 import { Cardprop } from './components/ui/Cardprop';
-<<<<<<< HEAD
 import { Progressbar } from './components/ui/Progressbar';
-=======
 import { CardContentContainer } from './components/ui/CardContentContainer';
->>>>>>> main
 
 function App() {
   return (
@@ -24,24 +21,14 @@ function App() {
         <Cardtag variant="poison">Poison</Cardtag>
         <Cardtag variant="grass">Grass</Cardtag>
         <Button>Grass</Button>
-<<<<<<< HEAD
-        <div className="max-w-xs">
-          <Cardprop value="888" label="Attack" />
-=======
         <CardContentContainer>
-          <Cardprop value="888">Attack</Cardprop>
-<<<<<<< HEAD
->>>>>>> main
-        </div>
-        <Progressbar currentValue={80} />
-=======
+          <Cardprop value="888" label="Attack" />
         </CardContentContainer>
-
+        <Progressbar currentValue={80} />
         <CardContentContainer>
           <span>Attack</span>
           <p>lorem ipsum lorem ipsumlorem ipsumlorem ipsum</p>
         </CardContentContainer>
->>>>>>> main
       </div>
     </div>
   );
