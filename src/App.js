cc7eccccd8bbcfa0821c103f03895d4e15b8d19f
Almir <aucanbarlic@axistms.net>
2024-02-12 09:19:53 +0100

    merge header with main

--- src/App.js
+++ src/App.js
@@ -5,29 +5,29 @@ import { Header } from './components/Header';
 import { Cardtag } from './components/ui/Cardtag';
 import { Button } from './components/ui/Button';
 import { Cardprop } from './components/ui/Cardprop';
+import { CardContentContainer } from './components/ui/CardContentContainer';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <div className="container">
-        <div>
-          <div className="grid grid-cols-12">
-            <Input
-              className="mb-2 col-span-4"
-              placeholder="Encuentra tu pokémon..."
-              variant="big"
-            />
-            <Input className="col-span-4" placeholder="Encuentra tu pokémon..." />
-            <Checkbox className="col-span-4" label="Electric" />
-          </div>
-          <Cardtag variant="poison">Poison</Cardtag>
-          <Cardtag variant="grass">Grass</Cardtag>
-          <Button>Grass</Button>
-          <div className="max-w-xs">
-            <Cardprop value="888">Attack</Cardprop>
-          </div>
+        <div className="grid grid-cols-12">
+          <Input className="mb-2 col-span-4" placeholder="Encuentra tu pokémon..." variant="big" />
+          <Input className="col-span-4" placeholder="Encuentra tu pokémon..." />
+          <Checkbox className="col-span-4" label="Electric" />
         </div>
+        <Cardtag variant="poison">Poison</Cardtag>
+        <Cardtag variant="grass">Grass</Cardtag>
+        <Button>Grass</Button>
+        <CardContentContainer>
+          <Cardprop value="888">Attack</Cardprop>
+        </CardContentContainer>
+
+        <CardContentContainer>
+          <span>Attack</span>
+          <p>lorem ipsum lorem ipsumlorem ipsumlorem ipsum</p>
+        </CardContentContainer>
       </div>
     </div>
   );
