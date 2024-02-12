d94e1e1144ef6eddbeed06990a01c07a40ad47d7
Almir <aucanbarlic@axistms.net>
2024-02-12 16:59:52 +0100

    fixing flex direction

--- src/components/ui/Horizontal.js
+++ src/components/ui/Horizontal.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const Horizontal = ({ children, align, justify }) => {
   return (
-    <div style={{ alignItems: `${align}`, justifyContent: `${justify}` }} className="flex flex-row">
+    <div style={{ alignItems: `${align}`, justifyContent: `${justify}` }} className="flex">
       {children}
     </div>
   );
