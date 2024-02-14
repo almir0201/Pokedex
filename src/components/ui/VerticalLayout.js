2ad37e5d71bd7b48a26ab8b90094be9007979dda
Almir <aucanbarlic@axistms.net>
2024-02-14 09:45:13 +0100

    fixed eslint error

--- src/components/ui/VerticalLayout.js
+++ src/components/ui/VerticalLayout.js
@@ -5,12 +5,12 @@ export const VerticalLayout = ({ children, align, justify, className }) => {
   return (
     <div
       className={cn('flex flex-col', className, {
-        'items-start': justify == 'start',
-        'items-end': justify == 'end',
-        'items-center': justify == 'center',
-        'justify-center': align == 'center',
-        'justify-start': align == 'start',
-        'justify-end': align == 'end'
+        'items-start': justify === 'start',
+        'items-end': justify === 'end',
+        'items-center': justify === 'center',
+        'justify-center': align === 'center',
+        'justify-start': align === 'start',
+        'justify-end': align === 'end'
       })}>
       {children}
     </div>
