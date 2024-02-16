a9b4be667adee479db6d0bf71c8890a1a53735ef
Almir <aucanbarlic@axistms.net>
2024-02-16 18:54:32 +0100

    aspect ratio wrapper fixes

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
         <div className="max-w-xs">
           <Cardprop value="888">Attack</Cardprop>
         </div>
-        <Ratiowrapper padding={87.44}>
+        <Ratiowrapper variant="1/1">
           <img src="/assets/charizard.png" alt="charizard" />
         </Ratiowrapper>
       </div>
--- src/components/ui/Ratiowrapper.js
+++ src/components/ui/Ratiowrapper.js
@@ -1,9 +1,23 @@
 import React from 'react';
 
-export const Ratiowrapper = ({ children, padding }) => {
+export const Ratiowrapper = ({ children, variant }) => {
+  let UIstyle = 'w-full h-0 relative overflow-hidden ';
+
+  if (variant === '1/1') {
+    UIstyle += 'pb-[100%]';
+  }
+  if (variant === '16/9') {
+    UIstyle += 'pb-[56.25%]';
+  }
+  if (variant === '4/3') {
+    UIstyle += 'pb-[75%]';
+  }
+  if (variant === '3/4') {
+    UIstyle += 'pb-[133.33%]';
+  }
   return (
-    <div style={{ paddingBottom: `${padding}%` }} className="w-full h-0">
-      {children}
+    <div className={UIstyle}>
+      <div className="w-full h-full absolute left-0">{children}</div>
     </div>
   );
 };
