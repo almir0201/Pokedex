7ac4619f788c10cd55f87d0ad908cb739b250f6a
Almir <aucanbarlic@axistms.net>
2024-02-18 16:32:50 +0100

    removed grid

--- src/components/ui/Types.js
+++ src/components/ui/Types.js
@@ -3,19 +3,15 @@ import { Checkbox } from './Checkbox';
 export const Types = ({ data }) => {
   return (
     <>
-    { data.length ?
-      <div className="grid grid-rows-6 grid-flow-col gap-1 md:grid-flow-row">
-        {data.map((type) => {
-          return (
-            <Checkbox
-              className=""
-              label={type}
-              key={`${type}-checkbox`}
-            />)
-        })}
-      </div> :
-      <></>
-    }
+      {data.length ? (
+        <div>
+          {data.map((type) => {
+            return <Checkbox label={type} key={`${type}-checkbox`} />;
+          })}
+        </div>
+      ) : (
+        <></>
+      )}
     </>
   );
 };
