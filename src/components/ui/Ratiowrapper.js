6ee2fc841ceef609eba38b36652ac77a3259a49f
Almir <aucanbarlic@axistms.net>
2024-02-18 16:28:50 +0100

    added direct child selectors

--- src/components/ui/Ratiowrapper.js
+++ src/components/ui/Ratiowrapper.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export const Ratiowrapper = ({ children, variant }) => {
-  let UIstyle = 'w-full h-0 relative overflow-hidden ';
+  let UIstyle = 'w-full h-full relative overflow-hidden [&>]:absolute [&>]:left-0 ';
 
   if (variant === '1/1') {
     UIstyle += 'pb-[100%]';
@@ -15,9 +15,5 @@ export const Ratiowrapper = ({ children, variant }) => {
   if (variant === '3/4') {
     UIstyle += 'pb-[133.33%]';
   }
-  return (
-    <div className={UIstyle}>
-      <div className="w-full h-full absolute left-0">{children}</div>
-    </div>
-  );
+  return <div className={UIstyle}>{children}</div>;
 };
