bf25f26beffa61363565af599236d8355ff8334e
Almir <aucanbarlic@axistms.net>
2024-02-21 17:42:52 +0100

    installed tanstack query

--- src/App.js
+++ src/App.js
@@ -13,6 +13,9 @@ import { ProgressWithDescription } from './components/ui/ProgressWithDescription
 import { Dropdown, Content, Item, Trigger } from './components/ui/Dropdown';
 import { Paragraph } from './components/ui/Paragraph';
 import arrowfilter from './icons/ArrowFilter.svg';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 function App() {
   const options = ['Fire', 'Normal', 'Electric', 'Water'];
@@ -25,51 +28,57 @@ function App() {
   };
 
   return (
-    <div className="App">
-      <Header />
-      <div className="container">
-        <div className="grid grid-cols-12">
-          <Input className="mb-2 col-span-4" placeholder="Encuentra tu pokémon..." variant="big" />
-          <Input className="col-span-4" placeholder="Encuentra tu pokémon..." />
-          <Checkbox className="col-span-4" label="Electric" />
+    <QueryClientProvider client={queryClient}>
+      <div className="App">
+        <Header />
+        <div className="container">
+          <div className="grid grid-cols-12">
+            <Input
+              className="mb-2 col-span-4"
+              placeholder="Encuentra tu pokémon..."
+              variant="big"
+            />
+            <Input className="col-span-4" placeholder="Encuentra tu pokémon..." />
+            <Checkbox className="col-span-4" label="Electric" />
+          </div>
+          <Dropdown>
+            <Trigger>
+              <Paragraph className="flex-1" variant="md" font="font-sanspro">
+                Type
+              </Paragraph>
+              <img className="mr-2" src={arrowfilter} alt="" />
+            </Trigger>
+            <Content>
+              {options.map((item, index) => (
+                <Item key={index}>
+                  <Checkbox
+                    className="col-span-4 mb-2 pl-7 leading-4"
+                    label={item}
+                    checked={checked[index]}
+                    onChange={() => handleCheckbox(index)}
+                  />
+                </Item>
+              ))}
+            </Content>
+          </Dropdown>
+          <Cardtag variant="poison">Poison</Cardtag>
+          <Cardtag variant="grass">Grass</Cardtag>
+          <Button>Grass</Button>
+          <CardContentContainer>
+            <Cardprop value="888" label="Attack" />
+          </CardContentContainer>
+          <AspectRatio variant="1/1">
+            <img src="/assets/charizard.png" alt="charizard" />
+          </AspectRatio>
+          <Progressbar currentValue={80} />
+          <CardContentContainer>
+            <span>Attack</span>
+            <p>lorem ipsum lorem ipsumlorem ipsumlorem ipsum</p>
+          </CardContentContainer>
+          <ProgressWithDescription title="Attack" label="2342342" value={80} />
         </div>
-        <Dropdown>
-          <Trigger>
-            <Paragraph className="flex-1" variant="md" font="font-sanspro">
-              Type
-            </Paragraph>
-            <img className="mr-2" src={arrowfilter} alt="" />
-          </Trigger>
-          <Content>
-            {options.map((item, index) => (
-              <Item key={index}>
-                <Checkbox
-                  className="col-span-4 mb-2 pl-7 leading-4"
-                  label={item}
-                  checked={checked[index]}
-                  onChange={() => handleCheckbox(index)}
-                />
-              </Item>
-            ))}
-          </Content>
-        </Dropdown>
-        <Cardtag variant="poison">Poison</Cardtag>
-        <Cardtag variant="grass">Grass</Cardtag>
-        <Button>Grass</Button>
-        <CardContentContainer>
-          <Cardprop value="888" label="Attack" />
-        </CardContentContainer>
-        <AspectRatio variant="1/1">
-          <img src="/assets/charizard.png" alt="charizard" />
-        </AspectRatio>
-        <Progressbar currentValue={80} />
-        <CardContentContainer>
-          <span>Attack</span>
-          <p>lorem ipsum lorem ipsumlorem ipsumlorem ipsum</p>
-        </CardContentContainer>
-        <ProgressWithDescription title="Attack" label="2342342" value={80} />
       </div>
-    </div>
+    </QueryClientProvider>
   );
 }
 
