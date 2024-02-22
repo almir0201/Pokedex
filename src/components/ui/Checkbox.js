4d658c35de2dd65ad15ac03767d35e5ad8e945e4
Almir <aucanbarlic@axistms.net>
2024-02-22 10:41:12 +0100

    fixing tailwind

--- src/components/ui/Checkbox.js
+++ src/components/ui/Checkbox.js
@@ -1,11 +1,9 @@
-import React from 'react';
+import { cn } from '../../lib/utils';
 import { forwardRef } from 'react';
 
 export const Checkbox = forwardRef(({ label, className, ...props }, ref) => {
-  let UIstyle = 'font-normal text-base leading-5';
-  UIstyle += ` ${className}`;
   return (
-    <div className={UIstyle} ref={ref}>
+    <div className={cn('font-normal text-base leading-5', className)} ref={ref}>
       <label>
         <input type="checkbox" className="w-3 h-3 mr-2 rounded-none border-dark" {...props} />
         {label}
