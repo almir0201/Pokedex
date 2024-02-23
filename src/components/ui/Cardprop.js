48346cd8347a9f7c0b0f7cdaa204e211df9e6604
Almir <aucanbarlic@axistms.net>
2024-02-23 14:10:04 +0100

    adding tailwind utils

--- src/components/ui/Cardprop.js
+++ src/components/ui/Cardprop.js
@@ -3,7 +3,7 @@ import { Paragraph } from './Paragraph';
 export const Cardprop = ({ label, value }) => {
   return (
     <>
-      <div className="relative rounded-full border-dark border-3 h-0 pb-[100%]">
+      <div className="bg-goldGradient relative rounded-full border-dark border-3 h-0 pb-[100%]">
         <span className="absolute inset-large">{value}</span>
       </div>
       <Paragraph className="text-center mt-2">{label}</Paragraph>
