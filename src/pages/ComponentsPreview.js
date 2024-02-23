43c6ae248aeaa576fedaf972bc33bb1389911058
Almir <aucanbarlic@axistms.net>
2024-02-23 14:59:36 +0100

    removed tanstack

--- src/pages/ComponentsPreview.js
+++ src/pages/ComponentsPreview.js
@@ -15,9 +15,6 @@ import { Dropdown, Content, Item, Trigger } from '../components/ui/Dropdown';
 import { Paragraph } from '../components/ui/Paragraph';
 import { Modal } from '../components/ui/Modal';
 import arrowfilter from '../icons/ArrowFilter.svg';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-
-const queryClient = new QueryClient();
 
 function ComponentsPreview() {
   const testArr = ['Bug', 'Fairy', 'Ghost', 'Dark', 'Fire', 'Glass', 'Lorem'];
@@ -40,68 +37,58 @@ function ComponentsPreview() {
   };
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <div className="App">
-        <Header />
-        <div className="container">
-          <div className="grid grid-cols-12">
-            <Input
-              className="mb-2 col-span-4"
-              placeholder="Encuentra tu pokémon..."
-              variant="big"
-            />
-            <Input className="col-span-4" placeholder="Encuentra tu pokémon..." />
-            <Checkbox className="col-span-4" label="Electric" />
-          </div>
-          <Dropdown>
-            <Trigger>
-              <Paragraph className="flex-1" variant="md" font="font-sanspro">
-                Type
-              </Paragraph>
-              <img className="mr-2" src={arrowfilter} alt="" />
-            </Trigger>
-            <Content>
-              {options.map((item, index) => (
-                <Item key={index}>
-                  <Checkbox
-                    className="col-span-4 mb-2 pl-7 leading-4"
-                    label={item}
-                    checked={checked[index]}
-                    onChange={() => handleCheckbox(index)}
-                  />
-                </Item>
-              ))}
-            </Content>
-          </Dropdown>
-          <button type="button" onClick={handleOpen}>
-            Click me!
-          </button>
-          <Modal isOpen={open} onClose={handleClose}>
-            <Input
-              className="mb-2 col-span-4"
-              placeholder="Encuentra tu pokémon..."
-              variant="big"
-            />
-          </Modal>
-          <Cardtag variant="poison">Poison</Cardtag>
-          <Cardtag variant="grass">Grass</Cardtag>
-          <Button>Grass</Button>
-          <CardContentContainer>
-            <Cardprop value="888" label="Attack" />
-          </CardContentContainer>
-          <Progressbar currentValue={80} />
-          <CardContentContainer>
-            <span>Attack</span>
-            <p>lorem ipsum lorem ipsumlorem ipsumlorem ipsum</p>
-          </CardContentContainer>
-          <Types data={testArr} />
-          <ProgressWithDescription title="Attack" label="2342342" value={80} />
-          <AspectRatio variant="1/1">
-            <img src="/assets/charizard.png" alt="charizard" />
-          </AspectRatio>
+    <div className="App">
+      <Header />
+      <div className="container">
+        <div className="grid grid-cols-12">
+          <Input className="mb-2 col-span-4" placeholder="Encuentra tu pokémon..." variant="big" />
+          <Input className="col-span-4" placeholder="Encuentra tu pokémon..." />
+          <Checkbox className="col-span-4" label="Electric" />
         </div>
+        <Dropdown>
+          <Trigger>
+            <Paragraph className="flex-1" variant="md" font="font-sanspro">
+              Type
+            </Paragraph>
+            <img className="mr-2" src={arrowfilter} alt="" />
+          </Trigger>
+          <Content>
+            {options.map((item, index) => (
+              <Item key={index}>
+                <Checkbox
+                  className="col-span-4 mb-2 pl-7 leading-4"
+                  label={item}
+                  checked={checked[index]}
+                  onChange={() => handleCheckbox(index)}
+                />
+              </Item>
+            ))}
+          </Content>
+        </Dropdown>
+        <button type="button" onClick={handleOpen}>
+          Click me!
+        </button>
+        <Modal isOpen={open} onClose={handleClose}>
+          <Input className="mb-2 col-span-4" placeholder="Encuentra tu pokémon..." variant="big" />
+        </Modal>
+        <Cardtag variant="poison">Poison</Cardtag>
+        <Cardtag variant="grass">Grass</Cardtag>
+        <Button>Grass</Button>
+        <CardContentContainer>
+          <Cardprop value="888" label="Attack" />
+        </CardContentContainer>
+        <Progressbar currentValue={80} />
+        <CardContentContainer>
+          <span>Attack</span>
+          <p>lorem ipsum lorem ipsumlorem ipsumlorem ipsum</p>
+        </CardContentContainer>
+        <Types data={testArr} />
+        <ProgressWithDescription title="Attack" label="2342342" value={80} />
+        <AspectRatio variant="1/1">
+          <img src="/assets/charizard.png" alt="charizard" />
+        </AspectRatio>
       </div>
-    </QueryClientProvider>
+    </div>
   );
 }
 
