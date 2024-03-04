725b61401a89e2d6b98bd2573dd59b1edb45c6c6
Almir <aucanbarlic@axistms.net>
2024-03-04 18:35:53 +0100

    fixed comments

--- src/components/ui/BaseCard.js
+++ src/components/ui/BaseCard.js
@@ -4,35 +4,42 @@ import { Cardprop } from './Cardprop';
 import { AspectRatio } from './AspectRatio';
 import { CardContentContainer } from './CardContentContainer';
 import { Paragraph } from './Paragraph';
+import { HorizontalLayout } from './HorizontalLayout';
 
 export const BaseCard = ({ title, imageSrc, imageAlt, powerStats, cardtagTypes }) => {
-  if (!powerStats.length) return null;
-  if (!cardtagTypes.length) return null;
+  if (!powerStats.length || !cardtagTypes.length) return null;
 
   return (
     <CardContentContainer>
-      <div className="flex justify-end mb-10 pl-6 relative">
-        <div className="mt-2 ml-6 absolute left-0 top-0 w-1/2 z-10">
-          <Paragraph className="font-bold leading-7">{title}</Paragraph>
-          <div className="flex [&>*]:flex-1 mt-2 mb-2 gap-3 [&>*]:max-w-[50px]">
+      <HorizontalLayout justify="end" className="mb-10 pl-6 relative">
+        <div className="absolute left-6 top-2 w-1/2 z-10">
+          <Paragraph variant="md" lineHeight="leading">
+            <b>{title && title}</b>
+          </Paragraph>
+          <HorizontalLayout align="start" justify="start" className="gap-3">
             {powerStats?.map((data, index) => (
-              <Cardprop key={index} value={data.value} label={data.label} />
+              <Cardprop
+                key={index}
+                value={data.value}
+                label={data.label}
+                className="flex-1 mt-2 mb-2 max-w-[50px]"
+              />
             ))}
-          </div>
-          <div className="flex gap-3 text-xs -mr-10 pb-3">
+          </HorizontalLayout>
+          <HorizontalLayout className="gap-3 text-xs pb-3">
             {cardtagTypes.map((data, index) => (
               <Cardtag key={index} variant={data.variant}>
                 {data.label}
               </Cardtag>
             ))}
-          </div>
+          </HorizontalLayout>
         </div>
-        <div className="red-gradient w-full basis-2/3 pr-4 pl-12 rounded-r-lg">
+        <div className="red-gradient basis-2/3 pr-4 pl-12 rounded-r-lg">
           <AspectRatio variant="1/1">
-            <img src={imageSrc} alt={imageAlt} />
+            <img src={imageSrc && imageSrc} alt={imageAlt && imageAlt} />
           </AspectRatio>
         </div>
-      </div>
+      </HorizontalLayout>
     </CardContentContainer>
   );
 };
--- src/components/ui/Paragraph.js
+++ src/components/ui/Paragraph.js
@@ -1,15 +1,17 @@
 import { cn } from '../../lib/utils';
 
-export const Paragraph = ({ className, children, variant, font }) => {
+export const Paragraph = ({ className, children, variant = 'default', font, lineHeight }) => {
   let fontFamily = font ?? 'font-roboto';
   return (
     <p
-      className={cn('text-base font-normal text-black', fontFamily, className, {
+      className={cn('text-base font-normal', fontFamily, lineHeight, className, {
         'text-xl hover:underline underline-offset-[15px]': variant === 'xl',
-        'text-xs text-dark': variant === 'xs',
+        'text-xs': variant === 'xs',
         'text-sm': variant === 'sm',
-        'text-lg text-dark': variant === 'lg',
-        'text-base': variant === 'md'
+        'text-lg': variant === 'lg',
+        'text-base': variant === 'default',
+        'text-md': variant === 'md',
+        'leading-7': lineHeight === 'leading'
       })}>
       {children}
     </p>
