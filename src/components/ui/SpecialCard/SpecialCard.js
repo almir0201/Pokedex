b2af1c8a24a75aa65a970fbffb1c145048b74b1b
almir0201 <59663344+almir0201@users.noreply.github.com>
2024-03-05 17:49:12 +0100

    Update SpecialCard.js

--- src/components/ui/SpecialCard/SpecialCard.js
+++ src/components/ui/SpecialCard/SpecialCard.js
@@ -1,5 +1,5 @@
-import { AspectRatio } from './AspectRatio';
-import { CardTitle } from './CardTitle';
+import { AspectRatio } from '../AspectRatio/AspectRatio';
+import { CardTitle } from '../CardTitle/CardTitle';
 
 export const SpecialCard = ({ title, icon, imageSrc, imageAlt }) => {
   return (
