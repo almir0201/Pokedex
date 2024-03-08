4407bb9759a70a9640f46f94b9fbde70960ea873
Almir <aucanbarlic@axistms.net>
2024-03-08 10:22:28 +0100

    fixes

--- src/components/ui/FilterStat.js
+++ src/components/ui/FilterStat.js
@@ -3,21 +3,11 @@ import { Input } from './Input';
 export const FilterStat = () => {
   return (
     <div className="flex justify-between items-center gap-4">
-      <Input
-        placeholder="70 000"
-        variant="sm"
-        label="From"
-        labelClassName="pl-4 mb-1 text-xs font-roboto"
-      />
+      <Input placeholder="70 000" variant="sm" label="From" />
 
       <p className="border-b-2 mt-6 w-6" />
 
-      <Input
-        placeholder="50 000"
-        variant="sm"
-        label="To"
-        labelClassName="pl-4 mb-1 text-xs font-roboto"
-      />
+      <Input placeholder="50 000" variant="sm" label="To" />
     </div>
   );
 };
