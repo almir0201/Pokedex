7284c727b56feda196333dcb6c1f9e7384ed90d0
Almir <aucanbarlic@axistms.net>
2024-03-08 12:13:01 +0100

    Dropdown stories

--- src/components/ui/Headline/Headline.js
+++ src/components/ui/Headline/Headline.js
@@ -1,4 +1,4 @@
-import { cn } from '../../lib/utils';
+import { cn } from '../../../lib/utils';
 
 export const Headline = ({ children, as: Component = 'h1', type = 'h1', className }) => (
   <Component
