70af8df1d35a346004c6f671b835815e1813bc9a
almir0201 <59663344+almir0201@users.noreply.github.com>
2024-03-09 14:05:38 +0100

    cardtitle stories (#94)
    Co-authored-by: Almir <aucanbarlic@axistms.net>

---
+++ src/stories/CardTitle.stories.js
@@ -0,0 +1,15 @@
+import { CardTitle } from '../components/ui/CardTitle/CardTitle';
+import Icon from '../icons/goldenPokeball.svg';
+
+export default {
+  title: 'Components/CardTitle',
+  component: CardTitle
+};
+
+const Template = (args) => <CardTitle {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  title: 'Default',
+  icon: Icon
+};
