56b638ceaa9427f6cfdbabe413a7473b9ce07f98
almir0201 <59663344+almir0201@users.noreply.github.com>
2024-03-09 14:07:25 +0100

    Dropdown stories (#85)
    Co-authored-by: Almir <aucanbarlic@axistms.net>

---
+++ src/stories/Dropdown.stories.js
@@ -0,0 +1,48 @@
+import { Dropdown, Content, Item, Trigger } from '../components/ui/Dropdown/Dropdown';
+import { Paragraph } from '../components/ui/Paragraph/Paragraph';
+import arrowfilter from '../icons/arrowFilter.svg';
+import { Checkbox } from '../components/ui/Checkbox/Checkbox';
+import { useState } from 'react';
+
+export default {
+  title: 'Components/Dropdown',
+  component: Dropdown
+};
+
+const Template = (args) => <Dropdown {...args} />;
+
+const DropdownWithHooks = () => {
+  const options = ['Fire', 'Normal', 'Electric', 'Water'];
+  const [checked, setChecked] = useState(new Array(options.length).fill(false));
+  const handleCheckbox = (position) => {
+    const updatedChecked = checked.map((item, index) => (index === position ? !item : item));
+    setChecked(updatedChecked);
+  };
+  return (
+    <Dropdown>
+      <Trigger>
+        <Paragraph className="flex-1" variant="md" font="font-sanspro">
+          Type
+        </Paragraph>
+        <img className="mr-2" src={arrowfilter} alt="" />
+      </Trigger>
+      <Content>
+        {options.map((item, index) => (
+          <Item key={index}>
+            <Checkbox
+              className="col-span-4 mb-2 pl-7 leading-4"
+              label={item}
+              checked={checked[index]}
+              onChange={() => handleCheckbox(index)}
+            />
+          </Item>
+        ))}
+      </Content>
+    </Dropdown>
+  );
+};
+
+export const Primary = Template.bind({});
+Primary.args = {
+  children: <DropdownWithHooks />
+};
