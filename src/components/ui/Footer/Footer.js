814b582740ed83a3710189ba73d0a3f0bd6346d9
almir0201 <59663344+almir0201@users.noreply.github.com>
2024-03-29 23:39:23 +0100

    Update Footer.js

--- src/components/ui/Footer/Footer.js
+++ src/components/ui/Footer/Footer.js
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-import { cn } from '../../../lib/utils';
 import { VerticalLayout } from '../VerticalLayout/VerticalLayout';
 import { Paragraph } from '../Paragraph/Paragraph';
 
@@ -21,11 +19,3 @@ export const Footer = ({ labelRight, labelLeft, className }) => {
       </div>
     </div>
   );
-=======
-import { HorizontalLayout } from '../HorizontalLayout/HorizontalLayout';
-import { cn } from '../../../lib/utils';
-
-export const Footer = ({ children, className }) => {
-  return <HorizontalLayout className={cn(className)}>{children}</HorizontalLayout>;
->>>>>>> main
-};
